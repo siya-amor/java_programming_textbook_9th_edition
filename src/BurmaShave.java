21e1ab7300bca6a1bcd3fc03f7a81e118b9780c8
siyagallant345@gmail.com <siyagallant345@gmail.com>
2025-05-06 10:03:01 +0200

    removed  the block comment in BurmaShave.java

--- src/BurmaShave.java
+++ src/BurmaShave.java
@@ -5,8 +5,8 @@ public class BurmaShave
     public static void main(String[]args)
     {
         JOptionPane.showMessageDialog(null, "Shaving Brushes");
-        /*JOptionPane.showMessageDialog(null,"You'll soon see 'em");
+        JOptionPane.showMessageDialog(null,"You'll soon see 'em");
         JOptionPane.showMessageDialog(null,"On a shelf");
-        JOptionPane.showMessageDialog(null,"In some museum");*/
+        JOptionPane.showMessageDialog(null,"In some museum");
     }
 }
